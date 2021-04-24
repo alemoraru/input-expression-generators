5d56a6544e7c1558fbbb0802e405b043674b49fa
Alexandru Moraru <A.Moraru@student.tudelft.nl>
2021-04-24 08:56:49 +0200

    Remove faulty structure

--- ChangeLog.md
+++
@@ -1,3 +0,0 @@
-# Changelog for quickcheck-interpreters
-
-## Unreleased changes
--- src/SimpleFunctions.hs
+++
@@ -1,8 +0,0 @@
-module SimpleFunctions where
-
--- Simple arithmetic functions
-add :: Int -> Int -> Int
-add x y = x + y
-
-mul :: Int -> Int -> Int
-mul x y = x * y
