746a9cc3a9ff9e31c0fdb0a372a5be0020f3d0d9
Alexandru Moraru <A.Moraru@student.tudelft.nl>
2021-05-09 14:41:09 +0200

    Try out random stuff

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,18 +1,24 @@
-import Arithmetic.Grammar
+import Arithmetic.Grammar ()
 import qualified Arithmetic.Interp1 as I1
 import qualified Arithmetic.Interp2 as I2
 import qualified Arithmetic.InterpFaulty1 as IF1
 
-import BTrees.Grammar
+import BTrees.Grammar ( Tree(Leaf, Branch) )
 import qualified BTrees.Interp1 as B1
 import qualified BTrees.Interp2 as B2
 import qualified BTrees.InterpFaulty1 as BF1
 
+import Test.QuickCheck
+
+lb :: Int
+lb = 1
+
+rb :: Int
+rb = 10
+
 main :: IO ()
 main = do
-    -- print (I1.interp (Add (Val 1) (Val 2)))
-    print (B1.interp (Branch (Leaf 1) (Leaf 2)))
-    print (B2.interp (Branch (Leaf 1) (Leaf 2)))
-    print (BF1.interp (Branch (Leaf 1) (Leaf 2)))
-
+    putStrLn "Generating a random integer:"
+    el <- generate $ frequency [(1, choose (1 :: Int, 10 :: Int)), (100, choose (11 :: Int, 20 :: Int)), (0, choose (21 :: Int, 30 :: Int))]
+    print el
     return ()
