e615ba65193793cd94d489e52abc36442eef6888
Alexandru Moraru <A.Moraru@student.tudelft.nl>
2021-05-11 00:53:07 +0200

    Add collect combinator for conditional tests

--- test/SpecCond.hs
+++ test/SpecCond.hs
@@ -12,11 +12,31 @@ preConditionInterp expr =
         Left err -> False 
         Right _  -> True
 
+-- Property for checking equivalent interpreters
+-- Also showcase distribution of values per depth of parse tree
 prop_correct_interp :: Expr -> Property  
-prop_correct_interp expr = preConditionInterp expr ==> I1.interp expr [] == I2.interp expr []
+prop_correct_interp expr = preConditionInterp expr ==> collect (depth expr) $ I1.interp expr [] == I2.interp expr []
 
+-- Property for checking non-equivalent properties
+-- Also showcase distribution of values per depth of parse tree
 prop_faulty_interp :: Expr -> Property 
-prop_faulty_interp expr = preConditionInterp expr ==> I1.interp expr [] == IF1.interp expr []
+prop_faulty_interp expr = preConditionInterp expr ==> collect (depth expr) $ I1.interp expr [] == IF1.interp expr []
+
+depth :: Expr -> Integer 
+depth (EInt _)  = 1
+depth (EBool _) = 1
+depth (Id _)    = 1
+depth (Add l r) = 1 + max (depth l) (depth r)
+depth (Mul l r) = 1 + max (depth l) (depth r)
+depth (Not e)   = 1 + depth e
+depth (Or l r)  = 1 + max (depth l) (depth r)
+depth (And l r) = 1 + max (depth l) (depth r)
+depth (Eq l r)  = 1 + max (depth l) (depth r)
+depth (Lt l r)  = 1 + max (depth l) (depth r)
+depth (Gt l r)  = 1 + max (depth l) (depth r)
+depth (Lambda _ e) = 1 + depth e
+depth (App l r)    = 1 + max (depth l) (depth r)
+depth (If b t f)   = 1 + max (depth b) (max (depth t) (depth f))
 
 main :: IO ()
 main = do
