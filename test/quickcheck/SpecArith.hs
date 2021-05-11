b2c012a4697807ce7c448e6c09c29508b90de355
Alexandru Moraru <A.Moraru@student.tudelft.nl>
2021-05-11 11:49:14 +0200

    Add more properties

--- test/quickcheck/SpecArith.hs
+++ test/quickcheck/SpecArith.hs
@@ -11,6 +11,12 @@ prop_correct_interp expr = I1.interp expr == I2.interp expr
 prop_faulty_interp :: Expr -> Bool 
 prop_faulty_interp expr = I1.interp expr == IF1.interp expr
 
+prop_addition :: Expr -> Expr -> Bool 
+prop_addition e1 e2 = I1.interp (Add e1 e2) == I2.interp (Add e1 e2)
+
+prop_incorrect_addition :: Expr -> Expr -> Bool 
+prop_incorrect_addition e1 e2 = I2.interp (Add e1 e2) == IF1.interp (Add e1 e2)
+
 main :: IO ()
 main = do
     putStrLn "Testing correct intepretation:"
@@ -18,5 +24,11 @@ main = do
 
     putStrLn "Testing incorrect interpretation:"
     quickCheck prop_faulty_interp
+    
+    putStrLn "Testing addition 1:"
+    quickCheck prop_addition
+
+    putStrLn "Testing addition 2:"
+    quickCheck prop_incorrect_addition
 
     return ()
