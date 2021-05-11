9ba96b384d92054167b97af7d1fe4138d1c157aa
Alexandru Moraru <A.Moraru@student.tudelft.nl>
2021-05-11 12:54:19 +0200

    Add smallcheck dependency & modify gitignore

---
+++ test/smallcheck/SpecArith.hs
@@ -0,0 +1,9 @@
+import Arithmetic.Grammar (Expr (..))
+import qualified Arithmetic.Interp1 as I1
+import qualified Arithmetic.Interp2 as I2
+import qualified Arithmetic.InterpFaulty1 as IF1
+
+main :: IO ()
+main = do
+
+    return ()
