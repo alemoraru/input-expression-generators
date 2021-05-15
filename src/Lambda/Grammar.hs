882675383b63adad999ba3ff74a2e0ad14ec265b
Alexandru Moraru <A.Moraru@student.tudelft.nl>
2021-05-15 16:42:31 +0200

    Add untyped lambda calculus grammar

---
+++ src/Lambda/Grammar.hs
@@ -0,0 +1,25 @@
+module Lambda.Grammar where
+
+type Var = String
+
+-- Reference untyped lambda calculus
+-- taken from Types & Programming Languages
+-- book by Benjamin C. Pierce
+data Expr = Id Var
+          | Lambda String Expr
+          | App Expr Expr
+          deriving (Eq)
+
+instance Show Expr where
+    show (Id x) = x
+    show (Lambda arg body) = "\\" ++ arg ++ "." ++ show body
+    show (App fun param)   = "(" ++ show fun ++ ") (" ++ show param ++ ")"
+
+sample1 :: Expr
+sample1 = Id "x"
+
+sample2 :: Expr
+sample2 = Lambda "x" (Id "x")
+
+sample3 :: Expr 
+sample3 = App (Lambda "x" (Id "x")) (Id "y")
