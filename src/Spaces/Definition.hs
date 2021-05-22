568876f03e9ea997db75ff4f1dc7e572af2cab3c
Alexandru Moraru <A.Moraru@student.tudelft.nl>
2021-05-22 14:35:23 +0200

    Add initial implementation of spaces

---
+++ src/Spaces/Definition.hs
@@ -0,0 +1,73 @@
+{-# LANGUAGE GADTs #-}
+
+module Spaces.Definition where
+    
+data Nat = Z | Suc Nat deriving (Show, Eq)
+data ListNat = Nill | Cons Nat ListNat deriving (Show, Eq)
+
+sizeNat :: Nat -> Int
+sizeNat Z       = 1
+sizeNat (Suc z) = 1 + sizeNat z
+
+sizeListNat :: ListNat -> Int
+sizeListNat Nill        = 1
+sizeListNat (Cons z zs) = 1 + sizeNat z + sizeListNat zs
+
+-- Definition of a GADT Space to represent ADTs
+data Space a where
+    Empty :: Space a
+    Pure  :: a -> Space a
+    (:+:) :: Space a -> Space a -> Space a
+    (:∗:) :: Space a -> Space b -> Space (a,b)
+    Pay   :: Space a -> Space a
+    (:$:) :: (a -> b) -> Space a -> Space b
+
+-- Produce a space of all applications of functions to params
+(<∗>) :: Space (a -> b) -> Space a -> Space b
+s1 <∗> s2 = (\(f ,a) -> f a) :$: (s1 :∗: s2)
+
+-- Space for Nats
+spaceNat :: Space Nat
+spaceNat = Pay (Pure Z :+: (Suc :$: spaceNat))
+
+-- Space for list of Nats
+spaceListNat :: Space ListNat
+spaceListNat = Pay (Pure Nill :+: (Cons :$: spaceNat <∗> spaceListNat))
+
+-- Data type for finite sets
+data FinSet a = EmptySet
+              | SingletonSet a
+              | DisjointSet (FinSet a) (FinSet a)
+              | CartesianSet (FinSet a) (FinSet a)
+
+instance Show a => Show (FinSet a) where
+    show EmptySet           = "{}"
+    show (SingletonSet x)   = "{" ++ show x ++ "}"
+    show (DisjointSet x y)  = show x ++ " U " ++ show y
+    show (CartesianSet x y) = show x ++ " x " ++ show y
+
+-- Comute the cardinality of a finite set
+card :: FinSet a -> Integer
+card EmptySet           = 0
+card (SingletonSet x)   = 1
+card (DisjointSet x y)  = card x + card y
+card (CartesianSet x y) = card x * card y
+
+-- Indexing function on the finite set type
+indexFin :: FinSet a -> Integer -> Maybe a
+indexFin EmptySet           i = Nothing
+indexFin (SingletonSet x)   i | i == 0     = Just x
+                              | otherwise  = Nothing
+indexFin (DisjointSet x y)  i | i < card x = indexFin x i
+                              | otherwise  = indexFin y (i - card x)
+indexFin (CartesianSet x y) i = undefined --(indexFin x (i `div` card y), indexFin y (i `mod` card y))
+
+-- extracts the finite set of values of a given size k from a space
+sized :: Space a -> Integer -> FinSet a
+sized Empty k    = EmptySet
+sized (Pure x) k | k == 0    = SingletonSet x
+                 | otherwise = EmptySet
+sized _ _ = undefined 
+
+indexSized :: Space a -> Int -> Integer -> a
+indexSized = undefined
