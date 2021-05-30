0edbf4728de93f3939547945d1239d8004012037
Alexandru Moraru <A.Moraru@student.tudelft.nl>
2021-05-30 13:28:56 +0200

    Add minor changes to the generator

--- src/Spaces/Generator.hs
+++ src/Spaces/Generator.hs
@@ -2,6 +2,12 @@ module Spaces.Generator where
 
 import Spaces.Grammar
 import Spaces.Definition
-import Test.QuickCheck
+import qualified Test.QuickCheck as QC
 
--- getLambdaTerm :: IO Term 
+-- Predicate that checks if a given term is of a specified type
+predicate :: Term -> Bool
+predicate = typeOf (TFun (TFun TInt TInt) TInt) 
+
+-- Get a random lambda term 
+getLambdaTerm :: IO Term 
+getLambdaTerm = QC.generate $ uniformFilter predicate spTerm 6
