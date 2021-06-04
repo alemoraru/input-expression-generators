d20ae5648d0dc3549f732c6e2016fe9c7a2624aa
Alexandru Moraru <A.Moraru@student.tudelft.nl>
2021-06-04 14:14:33 +0200

    Add more faulty interpreters for booleans package

---
+++ src/Booleans/InterpFaulty2.hs
@@ -0,0 +1,10 @@
+module Booleans.InterpFaulty2 where
+
+import Booleans.Grammar ( Expr(..) )
+
+-- Faulty interpreter for boolean expressions
+interp :: Expr -> Bool
+interp (Val x)          = x
+interp (And left right) = interp left && interp right
+interp (Or left right)  = interp left && interp right -- introduced a flaw here
+interp (Not expr)       = not $ interp expr
---
+++ src/Booleans/InterpFaulty3.hs
@@ -0,0 +1,10 @@
+module Booleans.InterpFaulty3 where
+
+import Booleans.Grammar ( Expr(..) )
+
+-- Faulty interpreter for boolean expressions
+interp :: Expr -> Bool
+interp (Val x)          = x
+interp (And left right) = interp left && interp right
+interp (Or left right)  = interp left || interp right
+interp (Not expr)       = interp expr -- introduced flaw here
