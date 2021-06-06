3ec9fb469327f37a58c4b97e944e2b585efdfbfc
Alexandru Moraru <A.Moraru@student.tudelft.nl>
2021-06-06 13:22:15 +0200

    Add interpreters for uniform generation approach

--- src/Booleans/Generator.hs
+++ src/Booleans/Generator.hs
@@ -39,3 +39,38 @@ arbExpr = uniformFilter (const True) spExpr 7
 -- Useful for QuickCheck properties
 instance Arbitrary Expr where
     arbitrary = arbExpr
+
+-- Correct interpreter
+interpC1 :: Expr -> Bool
+interpC1 (Val x)             = x
+interpC1 (And (left, right)) = interpC1 left && interpC1 right
+interpC1 (Or (left, right))  = interpC1 left || interpC1 right
+interpC1 (Not expr)          = not $ interpC1 expr
+
+-- Correct interpreter
+interpC2 :: Expr -> Bool
+interpC2 (Val x)             = x
+interpC2 (And (left, right)) = interpC2 right && interpC2 left
+interpC2 (Or (left, right))  = interpC2 right || interpC2 left
+interpC2 (Not expr)          = not $ interpC2 expr
+
+-- Incorrect interpreter
+interpF1 :: Expr -> Bool
+interpF1 (Val x)             = x
+interpF1 (And (left, right)) = interpF1 left || interpF1 right -- introduced flaw here
+interpF1 (Or (left, right))  = interpF1 left || interpF1 right
+interpF1 (Not expr)          = not $ interpF1 expr
+
+-- Incorrect interpreter
+interpF2 :: Expr -> Bool
+interpF2 (Val x)             = x
+interpF2 (And (left, right)) = interpF2 left && interpF2 right
+interpF2 (Or (left, right))  = interpF2 left && interpF2 right -- introduced flaw here
+interpF2 (Not expr)          = not $ interpF2 expr
+
+-- Incorrect interpreter
+interpF3 :: Expr -> Bool
+interpF3 (Val x)             = x
+interpF3 (And (left, right)) = interpF3 left && interpF3 right
+interpF3 (Or (left, right))  = interpF3 left || interpF3 right
+interpF3 (Not expr)          = interpF3 expr -- introduced flaw here
