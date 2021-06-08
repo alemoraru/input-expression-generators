cf6dd25dd4ba7ff647e34df35204e447b95f2a7e
Alexandru Moraru <A.Moraru@student.tudelft.nl>
2021-06-08 12:05:08 +0200

    Add more faulty conditional interpreters

--- src/Conditional/Generator.hs
+++ src/Conditional/Generator.hs
@@ -134,3 +134,99 @@ interpC1 (Gt (left, right)) = case (interpC1 left, interpC1 right) of
 interpC1 (If (i, (t, e))) = case interpC1 i of
     (Right (VBool cond)) -> if cond then interpC1 t else interpC1 e
     _ -> Left $ InterpError "Cannot evaluate non-boolean condition"
+
+-- Correct interpretation of conditional expressions
+interpC2 :: Expr -> Either Error Val
+interpC2 (EInt x)  = Right (VInt x)
+interpC2 (EBool x) = Right (VBool x)
+interpC2 (Add (left, right)) = case (interpC2 right, interpC2 left) of
+    (Right (VInt x), Right (VInt y)) -> Right (VInt $ x + y)
+    _ -> Left $ InterpError "Cannot perform addition on non-ints."
+interpC2 (Mul (left, right)) = case (interpC2 right, interpC2 left) of
+    (Right (VInt x), Right (VInt y)) -> Right (VInt $ x * y)
+    _ -> Left $ InterpError "Cannot perform multiplication on non-ints."
+interpC2 (Not expr) = case interpC2 expr of
+    (Right (VBool x)) -> Right (VBool $ not x)
+    _ -> Left $ InterpError "Cannot perform the not operation on non-booleans."
+interpC2 (Or (left, right)) = case (interpC2 right, interpC2 left) of
+    (Right (VBool x), Right (VBool y)) -> Right (VBool $ x || y)
+    _ -> Left $ InterpError "Cannot perform the or operation on non-booleans."
+interpC2 (And (left, right)) = case (interpC2 right, interpC2 left) of
+    (Right (VBool x), Right (VBool y)) -> Right (VBool $ x && y)
+    _ -> Left $ InterpError "Cannot perform the and operation on non-booleans."
+interpC2 (Eq (left, right)) = case (interpC2 right, interpC2 left) of
+    (Right (VInt x), Right (VInt y)) -> Right (VBool $ x == y)
+    _ -> Left $ InterpError "Cannot perform the and operation on non-booleans."
+interpC2 (Lt (left, right)) = case (interpC2 right, interpC2 left) of
+    (Right (VInt x), Right (VInt y)) -> Right (VBool $ x < y)
+    _ -> Left $ InterpError "Cannot perform comparison between non-ints."
+interpC2 (Gt (left, right)) = case (interpC2 right, interpC2 left) of
+    (Right (VInt x), Right (VInt y)) -> Right (VBool $ x > y)
+    _ -> Left $ InterpError "Cannot perform comparison between non-ints."
+interpC2 (If (i, (t, e))) = case interpC2 i of
+    (Right (VBool cond)) -> if cond then interpC2 t else interpC2 e
+    _ -> Left $ InterpError "Cannot evaluate non-boolean condition"
+
+-- Faulty interpretation of conditional expressions
+interpF1 :: Expr -> Either Error Val
+interpF1 (EInt x)  = Right (VInt x)
+interpF1 (EBool x) = Right (VBool x)
+interpF1 (Add (left, right)) = case (interpF1 left, interpF1 right) of
+    (Right (VInt x), Right (VInt y)) -> Right (VInt $ x + y)
+    _ -> Left $ InterpError "Cannot perform addition on non-ints."
+interpF1 (Mul (left, right)) = case (interpF1 left, interpF1 right) of
+    (Right (VInt x), Right (VInt y)) -> Right (VInt $ x * y)
+    _ -> Left $ InterpError "Cannot perform multiplication on non-ints."
+interpF1 (Not expr) = case interpF1 expr of
+    (Right (VBool x)) -> Right (VBool $ not x)
+    _ -> Left $ InterpError "Cannot perform the not operation on non-booleans."
+interpF1 (Or (left, right)) = case (interpF1 left, interpF1 right) of
+    (Right (VBool x), Right (VBool y)) -> Right (VBool $ x || y)
+    _ -> Left $ InterpError "Cannot perform the or operation on non-booleans."
+interpF1 (And (left, right)) = case (interpF1 left, interpF1 right) of
+    (Right (VBool x), Right (VBool y)) -> Right (VBool $ x && y)
+    _ -> Left $ InterpError "Cannot perform the and operation on non-booleans."
+interpF1 (Eq (left, right)) = case (interpF1 left, interpF1 right) of
+    (Right (VInt x), Right (VInt y)) -> Right (VBool $ x == y)
+    _ -> Left $ InterpError "Cannot perform the and operation on non-booleans."
+interpF1 (Lt (left, right)) = case (interpF1 left, interpF1 right) of
+    (Right (VInt x), Right (VInt y)) -> Right (VBool $ x < y)
+    _ -> Left $ InterpError "Cannot perform comparison between non-ints."
+interpF1 (Gt (left, right)) = case (interpF1 left, interpF1 right) of
+    (Right (VInt x), Right (VInt y)) -> Right (VBool $ x > y)
+    _ -> Left $ InterpError "Cannot perform comparison between non-ints."
+interpF1 (If (i, (t, e))) = case interpF1 i of
+    (Right (VBool cond)) -> if cond then interpF1 e else interpF1 t -- introduced flaw here (swapped branches)
+    _ -> Left $ InterpError "Cannot evaluate non-boolean condition"
+
+-- Faulty interpretation of conditional expressions
+interpF2 :: Expr -> Either Error Val
+interpF2 (EInt x)  = Right (VInt x)
+interpF2 (EBool x) = Right (VBool x)
+interpF2 (Add (left, right)) = case (interpF2 left, interpF2 right) of
+    (Right (VInt x), Right (VInt y)) -> Right (VInt $ x + y)
+    _ -> Left $ InterpError "Cannot perform addition on non-ints."
+interpF2 (Mul (left, right)) = case (interpF2 left, interpF2 right) of
+    (Right (VInt x), Right (VInt y)) -> Right (VInt $ x * y)
+    _ -> Left $ InterpError "Cannot perform multiplication on non-ints."
+interpF2 (Not expr) = case interpF2 expr of
+    (Right (VBool x)) -> Right (VBool $ not x)
+    _ -> Left $ InterpError "Cannot perform the not operation on non-booleans."
+interpF2 (Or (left, right)) = case (interpF2 left, interpF2 right) of
+    (Right (VBool x), Right (VBool y)) -> Right (VBool $ x || y)
+    _ -> Left $ InterpError "Cannot perform the or operation on non-booleans."
+interpF2 (And (left, right)) = case (interpF2 left, interpF2 right) of
+    (Right (VBool x), Right (VBool y)) -> Right (VBool $ x && y)
+    _ -> Left $ InterpError "Cannot perform the and operation on non-booleans."
+interpF2 (Eq (left, right)) = case (interpF2 left, interpF2 right) of
+    (Right (VInt x), Right (VInt y)) -> Right (VBool $ x == y)
+    _ -> Left $ InterpError "Cannot perform the and operation on non-booleans."
+interpF2 (Lt (left, right)) = case (interpF2 left, interpF2 right) of
+    (Right (VInt x), Right (VInt y)) -> Right (VBool $ x < y)
+    _ -> Left $ InterpError "Cannot perform comparison between non-ints."
+interpF2 (Gt (left, right)) = case (interpF2 left, interpF2 right) of
+    (Right (VInt x), Right (VInt y)) -> Right (VBool $ x > y)
+    _ -> Left $ InterpError "Cannot perform comparison between non-ints."
+interpF2 (If (i, (t, e))) = case interpF2 i of
+    (Right (VBool cond)) -> if cond then interpF2 t else interpF2 t -- introduced flaw here (always true branch)
+    _ -> Left $ InterpError "Cannot evaluate non-boolean condition"
