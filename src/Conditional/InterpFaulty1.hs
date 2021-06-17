5d4faf904401c4673a25fca317d3257e9b58cd31
Alexandru Moraru <A.Moraru@student.tudelft.nl>
2021-06-17 18:58:27 +0200

    Remove comments

--- src/Conditional/InterpFaulty1.hs
+++ src/Conditional/InterpFaulty1.hs
@@ -10,13 +10,10 @@ findId str ((name, val) : envRest) = if name == str then Right val else findId n
 
 -- Faulty interpretation of an expression 
 interp :: Expr -> Environment -> Either Error Val
-
--- basic building blocks
 interp (EInt x) nv  = Right (VInt x)
 interp (EBool b) nv = Right (VBool b)
 interp (Id str) nv  = findId str nv
 
--- basic operations on ints
 interp (Add (e1, e2)) nv =
   case (interp e1 nv, interp e2 nv) of
     (Right (VInt v1), Right (VInt v2)) -> Right (VInt (v1 + v2))
@@ -26,7 +23,6 @@ interp (Mul (e1, e2)) nv =
     (Right (VInt v1), Right (VInt v2)) -> Right (VInt (v1 * v2))
     _ -> Left $ InterpError "Cannot perform multiplication on non-ints."
 
--- basic operations on boolean
 interp (Not e) nv =
   case interp e nv of
     (Right (VBool v)) -> Right (VBool (not v))
@@ -40,25 +36,20 @@ interp (And (e1, e2)) nv =
     (Right (VBool v1), Right (VBool v2)) -> Right (VBool (v1 && v2))
     _ -> Left $ InterpError "Cannot perform and operation on non-booleans."
 
--- comparisons
 interp (Eq (e1, e2)) nv =
   case (interp e1 nv, interp e2 nv) of
     (Right (VInt v1), Right (VInt v2)) -> Right (VBool (v1 == v2))
     _ -> Left $ InterpError "Cannot perform equality check on non-ints."
-
 interp (Lt (e1, e2)) nv =
   case (interp e1 nv, interp e2 nv) of
     (Right (VInt v1), Right (VInt v2)) -> Right (VBool (v1 < v2))
     _ -> Left $ InterpError "Cannot perform number comparison on non-ints."
-
 interp (Gt (e1, e2)) nv =
   case (interp e1 nv, interp e2 nv) of
     (Right (VInt v1), Right (VInt v2)) -> Right (VBool (v1 > v2))
     _ -> Left $ InterpError "Cannot perform number comparison on non-ints."
 
--- functions
 interp (Lambda (str, e)) nv = Right (VClos (fst str) e nv)
-
 interp (App (e1, e2)) nv =
   case interp e1 nv of
     Right (VClos str e newEnv) ->
@@ -67,7 +58,6 @@ interp (App (e1, e2)) nv =
         _ -> Left $ InterpError "Cannot interpret body of lambda."
     _ -> Left $ InterpError "Cannot apply to non-closure type."
 
--- conditionals
 interp (If (e1, (e2, e3))) nv =
   case interp e1 nv of
     Right (VBool bool) -> if bool then interp e3 nv else interp e2 nv -- introduced error (swapped branches)
